efcd35a51b2d91bc901c4603247bde59afb6036e
Anmol Akhilesh <anmolakhilesh@gmail.com>
2019-02-14 22:01:44 +0530

    Update settings.py

--- polls/settings.py
+++ polls/settings.py
@@ -62,12 +62,12 @@ WSGI_APPLICATION = 'polls.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+# }
 
 
 # Password validation
